ef39fea95852363272478194bc5397fc16e78d11
Dominik <263915@student.pwr.edu.pl>
2024-04-17 11:42:49 +0200

    Add some bootstrap and render data from database

--- ridematefinderapp/src/app/home/home.component.ts
+++ ridematefinderapp/src/app/home/home.component.ts
@@ -1,24 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DriversService } from '../services/drivers.service';
 import { Driver } from '../../types';
 import { HttpErrorResponse } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  public drivers: Driver[] = [];
   constructor (
     private driversServices: DriversService
   ) {}
 
   ngOnInit() {
     this.driversServices.getDrivers('http://localhost:7777/driver').subscribe((response: Driver[]) => {
-      console.log(response);
+      this.drivers = response;
     },
     (error: HttpErrorResponse) => {
       alert(error.message);
